f8b17689d581fc3e1c6521438f18f7fef3cad518
Emma Mattner <emattner1997@gmail.com>
2021-07-24 22:15:42 +0930

    updating paths

--- react-portfolio/components/project.js
+++ react-portfolio/components/project.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import '../styles/project.css'
 
 
 const project = () => {
     return (
         <div className="container"></div>
-        
+
     );
 }
 

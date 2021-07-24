6869d3a15421efcf56725a29a965903f1d2b9e7c
Emma Mattner <emattner1997@gmail.com>
2021-07-24 22:16:27 +0930

    importing style

--- react-portfolio/components/footer.js
+++ react-portfolio/components/footer.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
+import '../styles/footer.css'
 
-const footer = () => {
+const Footer = () => {
     
 }
 
-export default footer
+export default Footer

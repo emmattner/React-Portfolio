eb7d22d7e82434d4a457a21039d8961c1348d000
Emma Mattner <emattner1997@gmail.com>
2021-07-24 22:15:54 +0930

    importing style

--- react-portfolio/components/navigation.js
+++ react-portfolio/components/navigation.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import '../styles/navigation.css'
 
-const navigation = () => {
+const Navigation = () => {
     
 }
 
 
-export default navigation
+export default Navigation
